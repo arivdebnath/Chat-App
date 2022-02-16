7ffcd0c7fafc935edcbc03542d3a1da899c1527d
Ariv Debnath <arivdebnath10@gmail.com>
2022-02-16 23:00:49 +0530

    added addUser & removeUser functions

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,12 +1,36 @@
-const user = {};
+const users = {};
 
-const addUser = ({id, username, roomname}) =>{
+const addUser = ({ id, username, roomname }) => {
     const username = username.trim();
     const roomname = roomname.trim();
 
-    if(!username||!roomname){
+    if (!username || !roomname) {
         return {
             error: "Invalid Username or Room",
         }
+    }
+
+    const existingUser = users.find((user) => {
+        return user.roomname === roomname && user.username === username;
+    })
+
+    if (existingUser) {
+        return {
+            error: "Username already in use!"
+        }
+    }
+
+    const user = { id, username, roomname };
+    users.push(user);
+    return { user };
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => {
+        return user.id === id;
+    })
+
+    if(index!==-1){
+        return users.splice(index, 1)[0];
     }
 }
